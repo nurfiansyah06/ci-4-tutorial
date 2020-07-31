41a0ab32bae2e65f85b27006b2a2a6fdad812a00
Muhamad Nur Arifiansyah <nurfiansyah06@gmail.com>
2020-07-31 22:14:15 +0700

    belajaran bro

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Coba extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	//--------------------------------------------------------------------
+
+}
